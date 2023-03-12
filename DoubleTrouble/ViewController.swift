4fad8ad0763110351fcf068f5a9e3c48298f0ec5
Cristi Sandu <cristisandu@csx.ro>
2023-03-12 22:56:02 +0200

    Add animation for dice roll

--- DoubleTrouble/ViewController.swift
+++ DoubleTrouble/ViewController.swift
@@ -12,17 +12,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    let diceArray = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+    var dice = [UIImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        for image in diceArray {
+            dice.append(UIImage(named: image)!)
+        }
+        
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        
+        animate(diceImageView: diceImageView1)
+        animate(diceImageView: diceImageView2)
+        
+        let a = Int.random(in: 0...5)
+        let b = Int.random(in: 0...5)
+        
+        diceImageView1.image = UIImage(named: diceArray[a])
+        diceImageView2.image = UIImage(named: diceArray[b])
+    }
+    
+    func animate(diceImageView: UIImageView) {
+        diceImageView.animationImages = dice
+        diceImageView.animationDuration = 0.5
+        diceImageView.animationRepeatCount = 2
+        diceImageView.startAnimating()
     }
     
 }
